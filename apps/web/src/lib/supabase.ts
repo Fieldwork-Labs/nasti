d122a2ab0ccee8400ae2b19c840bcd8f375629c1
Chid Gilovitz <chid@fieldwork.labs.xyz>
2025-03-06 15:16:06 +0800

    chore: add debugging info for env vars 2

--- apps/web/src/lib/supabase.ts
+++ apps/web/src/lib/supabase.ts
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js"
 import { Database } from "../types/database"
 
+console.log("ENV check:", {
+  directAccess: process.env.VITE_SUPABASE_URL,
+  viteAccess: import.meta.env.VITE_SUPABASE_URL,
+  allViteEnv: import.meta.env,
+})
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
