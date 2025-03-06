91c2b608c6ec0427c1b2fad657171352fe4f9a3a
Chid Gilovitz <chid@fieldwork.labs.xyz>
2025-03-06 17:05:59 +0800

    chore: remove unneeded stuff from vite.config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,25 +4,6 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import { nodePolyfills } from "vite-plugin-node-polyfills"
 import path from "path"
 
-const isProd = process.env.CF_PAGES === "1"
-let define = {}
-if (isProd) {
-  console.log(process.env)
-  console.log("0--------------9")
-  console.log("importmeta:", import.meta)
-  define = {
-    "import.meta.env.VITE_SUPABASE_URL": JSON.stringify(
-      process.env.VITE_SUPABASE_URL,
-    ),
-    "import.meta.env.VITE_SUPABASE_ANON_KEY": JSON.stringify(
-      process.env.VITE_SUPABASE_ANON_KEY,
-    ),
-    "import.meta.env.VITE_MAPBOX_ACCESS_TOKEN": JSON.stringify(
-      process.env.VITE_MAPBOX_ACCESS_TOKEN,
-    ),
-  }
-}
-
 export default defineConfig({
   resolve: {
     alias: {
@@ -34,7 +15,7 @@ export default defineConfig({
     process.env.NODE_ENV === "production"
       ? undefined
       : path.resolve(__dirname, "../.."),
-  define,
+  // define,
   plugins: [
     react({ jsxRuntime: "automatic" }),
     TanStackRouterVite(),
