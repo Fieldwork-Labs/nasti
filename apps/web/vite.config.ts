28c54e2e5b03294d65ad73c8a89b6ce963d93d49
Chid Gilovitz <chid@fieldwork.labs.xyz>
2025-03-06 16:29:50 +0800

    chore: manually define variables in prod

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,22 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import { nodePolyfills } from "vite-plugin-node-polyfills"
 import path from "path"
 
+const isProd = process.env.NODE_ENV === "production"
+let define = {}
+if (isProd) {
+  define = {
+    "import.meta.env.VITE_SUPABASE_URL": JSON.stringify(
+      process.env.VITE_SUPABASE_URL,
+    ),
+    "import.meta.env.VITE_SUPABASE_ANON_KEY": JSON.stringify(
+      process.env.VITE_SUPABASE_ANON_KEY,
+    ),
+    "import.meta.env.VITE_MAPBOX_ACCESS_TOKEN": JSON.stringify(
+      process.env.VITE_MAPBOX_ACCESS_TOKEN,
+    ),
+  }
+}
+
 export default defineConfig({
   resolve: {
     alias: {
@@ -11,10 +27,11 @@ export default defineConfig({
     },
   },
   // Use no envDir in production, default behavior works with Cloudflare
-  // envDir:
-  //   process.env.NODE_ENV === "production"
-  //     ? undefined
-  //     : path.resolve(__dirname, "../.."),
+  envDir:
+    process.env.NODE_ENV === "production"
+      ? undefined
+      : path.resolve(__dirname, "../.."),
+  define,
   plugins: [
     react({ jsxRuntime: "automatic" }),
     TanStackRouterVite(),
