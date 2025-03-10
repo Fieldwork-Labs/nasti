2b832eb4a35e4b74355a84f898cf63e5f0669e83
Chid Gilovitz <chid@fieldwork.labs.xyz>
2025-03-10 21:23:21 +0800

    chore: initial pwa setup

--- apps/pwa/vite.config.ts
+++ apps/pwa/vite.config.ts
@@ -1,36 +1,41 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa"
+import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "prompt",
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'nasti-pwa',
-      short_name: 'nasti-pwa',
-      description: 'nasti-pwa',
-      theme_color: '#092a0b',
-    },
+      manifest: {
+        name: "nasti-pwa",
+        short_name: "nasti-pwa",
+        description: "nasti-pwa",
+        theme_color: "#092a0b",
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
 })
