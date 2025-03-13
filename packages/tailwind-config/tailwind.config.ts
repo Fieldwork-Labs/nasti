bd515902ff3fbed1358f21a45a10577c963e68b1
Chid Gilovitz <chid@fieldwork.labs.xyz>
2025-03-13 21:09:58 +0800

    fix: tailwind container style issue

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -6,8 +6,6 @@ const config: Omit<Config, "content"> = {
   darkMode: "class",
   theme: {
     container: {
-      center: "true",
-      padding: "2rem",
       screens: {
         "2xl": "1400px",
       },
